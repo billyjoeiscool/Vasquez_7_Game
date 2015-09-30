3aa73087fb451c030b215646ff765381fc94d4fc
Joel Vasquez <feardeathd5@hotmail.com>
2015-09-30 10:36:59 -0500

    Hello world coolguy

---
+++ NetBeansProjects/Prime/src/prime/Prime.java
@@ -0,0 +1,30 @@
+
+// * To change this license header, choose License Headers in Project Properties.
+ //* To change this template file, choose Tools | Templates
+ //* and open the template in the editor.
+ /*
+//package prime;
+
+/**
+ *
+ * @author billyjoel
+ */
+//public class Prime {
+
+    /**
+     * @param args the command line arguments
+     */
+   // publ//ic static void main(String[] args) {
+        // TODO code application logic here
+        //int i=1;
+               //for(i=2; i <100; i++)         
+         // String e"2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83";              
+public static void main(String[] arg){
+for (int i= 0;)
+                    }
+
+    
+                    }
+    }
+    
+}
