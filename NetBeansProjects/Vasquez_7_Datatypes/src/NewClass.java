7964b1e230bcc659265dbe652501d9685e4e4e89
Joel Vasquez <feardeathd5@hotmail.com>
2015-09-30 10:44:06 -0500

    Hello world coolguy

---
+++ NetBeansProjects/Vasquez_7_Datatypes/src/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author billyjoel
+ */
+public class NewClass {
+    int X = 0;
+    for(int i = 0;i<100;i++) {
+if(isPrime)
+    
+        
+    
---
+++ NetBeansProjects/Vasquez_7_Datatypes/src/vasquez_7_datatypes/Vasquez_7_Datatypes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vasquez_7_datatypes;
+
+/**
+ *
+ * @author billyjoel
+ */
+public class Vasquez_7_Datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
