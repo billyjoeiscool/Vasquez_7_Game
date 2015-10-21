d66ac1cea2725f4b74a41d6a5dd04317413d39ad
Joel Vasquez <feardeathd5@hotmail.com>
2015-10-21 11:14:51 -0500

    I tryed my best

--- NetBeansProjects/Vasquez_7_MethodsMadness.java/src/vasquez_7_methodsmadness/java/Vasquez_7_MethodsMadessjava.java
+++ NetBeansProjects/Vasquez_7_MethodsMadness.java/src/vasquez_7_methodsmadness/java/Vasquez_7_MethodsMadessjava.java
@@ -48,8 +48,7 @@ public class Vasquez_7_MethodsMadessjava extends Application {
   
          gc.setFill(Color.BLACK);
         
-        gc.setLineWidth(20);
-        gc.strokeLine(40, 10, 10, 40);
+        
         gc.fillOval(150, 60, 30, 30);
         gc.strokeOval(90,80, 90,90); 
     
@@ -57,8 +56,8 @@ public class Vasquez_7_MethodsMadessjava extends Application {
         gc.setFill(Color.YELLOW);
      
         gc.setLineWidth(10);
-        gc.strokeLine(300, 45, 10, 70);
-        gc.fillOval(30, 80, 30, 30);
+        gc.strokeLine(300, 45,90, 70);
+       
         int i = 0;
        
 
@@ -69,24 +68,53 @@ public class Vasquez_7_MethodsMadessjava extends Application {
         for (i =0; i < 51000; i++) {
 if (i/6==0){
             
-   gc.setFill(Color.BLACK);   
-        gc.fillOval(100, 200, 100, 30);
-   
+  
+   cheek(gc);
+necklace(gc);
+M3(gc);
+M4(gc);
+M5(gc);
+    }
+        }
+    }
+    public void cheek(GraphicsContext gc ) {
+        gc.setFill(Color.RED);   
+            
+   gc.fillOval(200,80, 30, 30);
+        }
 
+    
+    public void necklace(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillOval(150,180,500,100);
+        gc.fillOval(200,180,500,100);
+        gc.fillOval(100,180,500,100);
+        gc.fillOval(50,180,500,100);
+        gc.fillOval(0,180,500,100);
     }
-    }
-    }
-    public void M1(GraphicsContext gc ){
-        for (int i = 0; i < 500; i++) {
-        if (i / 10 == 0) {
-        gc.setFill(Color.CYAN);   
-            gc.fillOval(200, 200, 100, 100);
-        }
-        }
-    }
+
+
+ public void M3(GraphicsContext gc) {
+ gc.setFill(Color.WHITE);
+ gc.fillOval(300,200,15,30);
+ gc.fillOval(250,200,15,30);
+ gc.fillOval(200,200,15,30);
+ gc.fillOval(100,200,15,30);
+ gc.fillOval(50,200,15,30);
+ }
+    
+
+ public void M4(GraphicsContext gc) {
+     gc.setFill(Color.WHITE);
+     gc.fillOval(90, 68, 10, 20);
+    gc.fillOval(155, 65, 10, 20);
+ }    
+
+
+public void M5(GraphicsContext gc) {
+  gc.setStroke(Color.BLACK); 
+  gc.strokeOval(0,0, 550,500);
+
 }
 
-    
-    
-
-    
+}
